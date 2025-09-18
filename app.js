d2d4ab36010cbf80ecf5c295a63da068157ce878
Katy Wells <katy.wells@verizon.net>
2025-09-18 15:12:00 -0400

    finished recursive function to run transformations, but still not working

--- app.js
+++ app.js
@@ -1,81 +1,98 @@
 // Pipeline Playground - Starter
 // =============================
 
-// TODO: Create an object to hold your transforms (e.g., toUpperCase, trim, appendExclamation)
+let inputText; //global variable to hold the string input by the user
 
-// the object holds the functions
-// MAYBE need input which will update after each method
-let inputText;
-const transformObj ={
-    capitalize: function() {
-       return inputText.toUpperCase();
-    },
-    trimIt: function() {
-        return inputText.trim();
-    },
-    appendExclamation: function() {
-       const newString = inputText + '!';
-       return newString;
-    }
-
+// object to hold transormation methods
+const transformObj = {
+  capitalize: function () {
+    return inputText.toUpperCase();
+  },
+  trimIt: function () {
+    return inputText.trim();
+  },
+  appendExclamation: function () {
+    const newString = inputText + '!';
+    return newString;
+  },
 };
 // TODO: Write your createPipeline function here
 // - It should let you add step functions
 // - It should let you run them all in order on some input
 
-
-// CLOSURE: when buttons are clicked, corresponding function needs to be added to a pipeline array so they can be run in order
-
-function createPipeline(input) {// input is inputText
-   const stepArr = [];
-   return {//returns obj with .add(fn) and .run(initialValue)
-    add: function(fn) { //fn is arg of function that was clicked
-        //Event - when button is clicked, add fn to array
-
+//STEP 3
+function createPipeline(input) {
+  // input is inputText
+  const stepArr = [];
+  return {
+    //returns obj with .add(fn) and .run(initialValue)
+    add: function (fn) {
+      //fn is arg of function that was clicked
+      stepArr.push(fn); //Event - when button is clicked, add fn to array
     },
-    run: function(initialValue) {}
-   }
-    
+    run: function transformer(initialValue, input) {
+      // recursion (initialValue is the stepArr)
+
+      if (!input) {
+        alert(
+          `Type something in the form and click the "submit" button.  Then choose how you want to transform your text.`
+        ); // conditional to check if there is input to transform
+      }
+      if (!initialValue[0]) {
+        //base: input array is empty
+        return input;
+      } else {
+        //recursive: reassign input to the result of invoking func at index 0
+        const transformedInput = initialValue[0](input);
+        return transformer(initialValue.slice(1), transformedInput);
+        //return the function with a diff array index and transformed inputText
+      }
+    },
+  };
 }
 
-// variables for the form and input
+const myPipeline = createPipeline(...args);
+
+//STEP 1 - when user types in the form and clicks submit, inputText is reassigned to the value of their input string
 const userForm = document.getElementById('userForm');
-const userInput = document.getElementById("userText");
+const userInput = document.getElementById('userText');
+userForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formInput = userInput.value;
+  inputText = formInput;
+  return inputText;
+});
 
-
-userForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formInput = userInput.value;
-   inputText = formInput;
-   return inputText;
-})
-
-// create variables for JS to note if button is clicked
-// take inputText 
-// transform inputText, then reasign inputText val 
-/*AM I DOING THIS RIGHT??
+/*
 Each of these variables checks if button is clicked and, if so, invokes the add method in createPipline
-NEXT: make the add method add the variable to the array*/
-const capButton = document.getElementById('uppercase-button').addEventListener('click', function() {
-   createPipeline.add(transformObj.capitalize(inputText));    
-});
-const trimButton = document.getElementById('trim-button').addEventListener('click', function() {
-    createPipeline.add(transformObj.trimIt(inputText));
-});
-const appendButton = document.getElementById('append-button').addEventListener('click', function() {
-    createPipeline.add(transformObj.appendExclamation(inputText));
-});
-// for each button, add event listener with params of "click", function () -> whatever should happen when clicked
-const runButton = document.getElementById('run-button').addEventListener('click', function() {
-    if (!inputText) {
-        // print message telling user to input something before pressing run button
-    } else { // call each function in the array in order, passing the updated inputText in to each
+*/
+//STEP 2
+const capButton = document.getElementById('uppercase-button');
+capButton.addEventListener('click', function () {
+    myPipeline.add(transformObj.capitalize);
+  });
+const trimButton = document.getElementById('trim-button');
+trimButton.addEventListener('click', function () {
+    myPipeline.add(transformObj.trimIt);
+  });
+const appendButton = document.getElementById('append-button');
+  appendButton.addEventListener('click', function () {
+    myPipeline.add(transformObj.appendExclamation);
+  });
 
-    }
+function changeOutput(text) {
+  // func to change existing text in output area to transformed text
+  const preElem = document.getElementById('output');
+  preElem.textContent = text;
+}
 
-});
-
-
+//MAYBE need to change order for run button
+//STEP 4
+const runButton = document
+  .getElementById('run-button')
+  .addEventListener('click', function () {
+    changeOutput(myPipeline.run(stepArr, inputText));
+  });
 
 // TODO: Wire up the DOM
 // - Grab the input box, buttons, step list, and output
